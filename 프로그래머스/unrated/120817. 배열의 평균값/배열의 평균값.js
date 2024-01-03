d39d832e5a2035038d55a4f441924eda8550fca1
ooheunda <ooheunda@gmail.com>
2024-01-03 12:42:55 +0900

    [level unrated] Title: 배열의 평균값, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    var answer = 0;
+    
+    for (let i = 0; i < numbers.length; i++) {
+        answer += numbers[i];
+    }
+    
+    answer /= numbers.length;
+    
+    return answer;
+}
