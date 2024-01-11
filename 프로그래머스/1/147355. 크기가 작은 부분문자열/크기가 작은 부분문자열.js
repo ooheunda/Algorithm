4fdd259720a2d0b76251108c87aa02a886d90354
ooheunda <ooheunda@gmail.com>
2024-01-11 10:18:03 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,13 @@
+function solution(t, p) {
+    let answer = 0;
+    
+    for (let i = 0; i < t.length - p.length + 1; i++) {
+        let str = '';
+        for (let j = i; str.length !== p.length; j++) {
+            str += t[j];
+        }
+        if (+str <= +p) answer++
+    }
+    
+    return answer;
+}
