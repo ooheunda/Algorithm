0178734dc20a284356889ab06f069b057fc1d964
ooheunda <ooheunda@gmail.com>
2024-01-18 00:05:53 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,15 +1,11 @@
 function solution(food) {
-    let str1 = [];
-    let str2 = [];
+    let str = [];
     
     for (let i = 1; i < food.length; i++) {
         for (let j = 0; j < Math.floor(food[i] / 2); j++) {
-            str1.push(i);
-            str2.push(i);
+            str.push(i);
         }
     }
     
-    str2.sort((a, b) => b - a);
-    
-    return str1.join('') + '0' + str2.join('');
+    return str.join('') + '0' + str.reverse().join('');
 }
