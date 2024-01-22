0aecf1bb9aa8786385225bbbf73244b40689c198
ooheunda <ooheunda@gmail.com>
2024-01-22 09:19:44 +0900

    [level 1] Title: 모의고사, Time: 4.72 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -3,16 +3,16 @@ function solution(answers) {
     let arr1 = [1, 2, 3, 4, 5];
     let arr2 = [2, 1, 2, 3, 2, 4, 2, 5];
     let arr3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-    let cnt1 = 0, cnt2 = 0, cnt3 = 0;
+    let cnt = [0, 0, 0]
     
     for (let i = 0; i < answers.length; i++) {
-        if (answers[i] === arr1[i % arr1.length]) cnt1++;
-        if (answers[i] === arr2[i % arr2.length]) cnt2++;
-        if (answers[i] === arr3[i % arr3.length]) cnt3++;
+        if (answers[i] === arr1[i % arr1.length]) cnt[0]++;
+        if (answers[i] === arr2[i % arr2.length]) cnt[1]++;
+        if (answers[i] === arr3[i % arr3.length]) cnt[2]++;
     }
     
-    [cnt1, cnt2, cnt3].reduce((_, cur, i) => {
-        if (cur === Math.max(cnt1, cnt2, cnt3)) answer.push(i + 1);
+    cnt.reduce((_, cur, i) => {
+        if (cur === Math.max(...cnt)) answer.push(i + 1);
     }, 0)
     
     return answer;
