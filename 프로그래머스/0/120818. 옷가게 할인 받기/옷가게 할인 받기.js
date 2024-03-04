44befb01ed481b739c0c1e9e675ebda5302a3033
ooheunda <ooheunda@gmail.com>
2024-03-04 09:29:55 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,11 @@
+function solution(price) {
+    if (price >= 500000) {
+        price -= price * 0.2;
+    } else if (price >= 300000) {
+        price -= price * 0.1;
+    } else if (price >= 100000) {
+        price -= price * 0.05;
+    }
+    
+    return Math.trunc(price);
+}
