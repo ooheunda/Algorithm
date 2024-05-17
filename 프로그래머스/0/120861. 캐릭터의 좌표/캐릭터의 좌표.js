5742d16d0bdbd27acb1c176fcd608dc03f8467af
ooheunda <ooheunda@gmail.com>
2024-05-17 11:45:19 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -3,16 +3,19 @@ function solution(keyinput, board) {
     const [maxX, maxY] = [Math.floor(board[0] / 2), Math.floor(board[1] / 2)];
     
     for (let i = 0; i < keyinput.length; i++) {
-        if (['up', 'down'].includes(keyinput[i])) {
-            answer[1] += keyinput[i] === 'up' ? 1 : -1;
-            if (answer[1] > maxY || answer[1] < -maxY) {
-                answer[1] = answer[1] > 0 ? maxY : -maxY;
-            }
-        } else {
-            answer[0] += keyinput[i] === 'right' ? 1 : -1;
-            if (answer[0] > maxX || answer[0] < -maxX) {
-                answer[0] = answer[0] > 0 ? maxX : -maxX;
-            }
+        switch (keyinput[i]) {
+            case 'up':
+                if (answer[1] < maxY) answer[1]++;
+                break;
+            case 'down':
+                if (answer[1] > -maxY) answer[1]--;
+                break;
+            case 'right':
+                if (answer[0] < maxX) answer[0]++;
+                break;
+            case 'left':
+                if (answer[0] > -maxX) answer[0]--;
+                break;
         }
     }
     
