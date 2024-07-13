d2067ac28c5c0e6f18f26aadf9bdc9b0e5b20771
ooheunda <ooheunda@gmail.com>
2024-07-13 23:26:30 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -0,0 +1,8 @@
+function solution(num_list) {
+    let a = num_list[num_list.length - 1];
+    let b = num_list[num_list.length - 2];
+    
+    num_list.push(a > b ? a - b : a * 2);
+    
+    return num_list;
+}
