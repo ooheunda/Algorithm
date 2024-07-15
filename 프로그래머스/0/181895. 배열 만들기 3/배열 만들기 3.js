cb4f72779a0d118e4ac806573f281adc7bf70173
ooheunda <ooheunda@gmail.com>
2024-07-15 16:24:46 +0900

    [level 0] Title: 배열 만들기 3, Time: 4.39 ms, Memory: 43.9 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,9 +1,5 @@
 function solution(arr, intervals) {
-    const answer = [];
+    const [[a1, b1], [a2, b2]] = intervals;
     
-    for (let i = 0; i < intervals.length; i++) {
-        answer.push(arr.filter((elem, idx) => intervals[i][0] <= idx && intervals[i][1] >= idx))
-    }
-    
-    return answer.flat();
+    return [...arr.slice(a1, b1 + 1), ...arr.slice(a2, b2 + 1)];
 }
