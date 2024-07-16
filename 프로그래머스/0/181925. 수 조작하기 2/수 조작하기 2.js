723ae4ef64c083734023f43ebf170b760fc25224
ooheunda <ooheunda@gmail.com>
2024-07-16 19:27:25 +0900

    [level 0] Title: 수 조작하기 2, Time: 11.85 ms, Memory: 45.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,22 @@
+function solution(numLog) {
+    let answer = '';
+    
+    for (let i = 0; i < numLog.length - 1; i++) {
+        switch (numLog[i + 1] - numLog[i]) {
+            case 1:
+                answer += 'w';
+                break;
+            case -1:
+                answer += 's';
+                break;
+            case 10:
+                answer += 'd';
+                break;
+            case -10:
+                answer += 'a';
+                break;
+        }
+    }
+    
+    return answer;
+}
