00fed9299ade23f44dd0d5543e1978c5bc153b73
ooheunda <ooheunda@gmail.com>
2024-07-18 15:38:15 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.41 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -0,0 +1,10 @@
+function solution(arr, queries) {
+    queries.forEach((elem) => {
+        let [i, j] = elem;
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    })
+    
+    return arr;
+}
