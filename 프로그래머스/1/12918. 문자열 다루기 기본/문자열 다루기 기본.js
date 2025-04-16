12c8e0e15edd21d2bf36074cf264b3f8ba701611
ooheunda <ooheunda@gmail.com>
2025-04-16 23:47:52 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,4 +1,4 @@
 function solution(s) {
     if (s.includes('e')) return false;
-    return (s.length === 4 || s.length === 6) && !isNaN(+s);
+    return (s.length === 4 || s.length === 6) && !isNaN(s);
 }
