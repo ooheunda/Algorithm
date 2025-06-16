56b282905e27f0dc023fd875c1cec88832919464
ooheunda <ooheunda@gmail.com>
2025-06-16 23:41:27 +0900

    [Bronze II] Title: 거스름돈, Time: 92 ms, Memory: 9320 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.js
+++ 백준/Bronze/5585. 거스름돈/거스름돈.js
@@ -1,35 +1,12 @@
 const input = require('fs').readFileSync(0, 'utf-8').toString().trim();
 
+const coins = [500, 100, 50, 10, 5, 1];
 let change = 1000 - input;
 let cnt = 0;
 
-if (change >= 500) {
-    cnt++;
-    change -= 500;
-}
-
-if (change >= 100) {
-    cnt += Math.floor(change / 100);
-    change = change % 100;
-} 
-
-if (change >= 50) {
-    cnt++;
-    change -= 50;
-} 
-
-if (change >= 10) {
-    cnt += Math.floor(change / 10);
-    change = change % 10;
-}
-
-if (change >= 5) {
-    cnt++;
-    change -= 5;
-}
-
-if (change >= 1) {
-    cnt += change;
-}
+coins.forEach((coin) => {
+    cnt += Math.floor(change / coin);
+    change = change % coin;
+})
 
 console.log(cnt);
