9ffc2a6ae5050090de97794f98e368f011e104d5
ooheunda <ooheunda@gmail.com>
2026-09-10 01:01:11 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -2,7 +2,6 @@ def solution(num_list):
     answer = [0, 0]
     
     for n in num_list:
-        is_even = n % 2
-        answer[1 if is_even else 0] += 1
+        answer[n % 2] += 1
         
     return answer
